e41b6ac36a2593c09f59d5ea5a58ccc2d9c598ca
pythonsemicolon <taz@tazs-Mac-mini.local>
2023-08-01 11:51:01 -0400

    Rewrite Zotero scripts Modified python scripts in similar manner to DDMAL website, added JSON content files and supporting JS, edited markup to accommodate dynamic content. Also made some async js functions allowing for the bolding of names to match previous references page.

--- html_parser.py
+++ html_parser.py
@@ -1,15 +1,11 @@
 from bs4 import BeautifulSoup
-from urllib.parse import unquote
-import re
-import os
-import markdown
-import shutil
+import json
 
-print('Media (m,M), presentations (pr, PR), publications (pu, PU), or all (a,A)?\n')
+print('References (r, R), or all (a,A)?\n')
 choice = str(input()).lower()
 
-input_list = ['m', 'pr', 'pu', 'a']
-full_list = ['media', 'presentations', 'publications']
+input_list = ['r', 'a']
+full_list = ['references']
 parse_list = []
 
 if choice not in input_list:
@@ -20,65 +16,25 @@ if choice == 'a':
 else:
     parse_list = [full_list[input_list.index(choice)]]
 
-simssa_root_folder = './'
+root_folder = './'
 export_folder = 'zotero_export/'
-
-# if os.path.exists():
-#     shutil.rmtree(simssa_root_folder + citation_folder + '/' + year)
 
 for type in parse_list:
 
-    html_file_name = f'SIMSSA_{type}.html'
-    citation_folder = f'_{type}'
+    html_file_name = f'LinkedMusic_{type}.html'
+    path = f'{type}/content.json'
 
     with open(export_folder + html_file_name) as f:
         html_soup = BeautifulSoup(f, 'html.parser')
 
-    shutil.rmtree(citation_folder)
-    os.makedirs(citation_folder)
-    # Save html (div) and ascii title [ [<div></div>, "Example Title"]]
-
-    html_array = []
+    content_array = []
 
     for html_tag in html_soup.findAll('div', {'class': 'csl-entry'}):
-        # print(tag.find_next('span'))
-        # if html_tag.find('a'):
-        #     continue
-        parse_attr = html_tag.find_next('span')['title']
-        year = 'n.d.'
-        author = 'no_author'
-        title = ')no_title'
-        a_title = ')no_a_title'
-        b_title = ')no_b_title'
-        if 'rft.date' in parse_attr:
-            year = parse_attr.split('rft.date=')[1].split('-')[0].split('&')[0]
-        if 'rft.aulast' in parse_attr:
-            author = unquote(parse_attr.split('rft.aulast=')[1].split('&')[0])
-        if 'rft.title' in parse_attr:
-            title = unquote(parse_attr.split('rft.title=')[1].split('&')[0])
-        if 'rft.atitle' in parse_attr:
-            a_title = unquote(parse_attr.split('rft.atitle=')[1].split('&')[0])
-        if 'rft.btitle' in parse_attr:
-            b_title = unquote(parse_attr.split('rft.btitle=')[1].split('&')[0])
+        content_array.append(str(html_tag))
 
-        final_title = ''
-        for t in [title, a_title, b_title]:
-            if t.split('_')[0] != ')no':
-                final_title = t
-                break
-        file_name = author + '_' + final_title.replace(' ', '_') + '_' + year + '.md'
+    # sort alphabetically by author
+    print(content_array)
+    content_array.sort()
 
-        if not os.path.exists(simssa_root_folder + citation_folder + '/' + year):
-            os.makedirs(simssa_root_folder + citation_folder + '/' + year)
-        with open(simssa_root_folder + citation_folder + '/' + year + '/' + file_name, 'w') as f:
-            f.write(f'---\npresentation_year: {year}\nyear: {year}\n---\n\n{html_tag.decode_contents()}')
-
-        print(html_tag.decode_contents(), '\n')
-        print(parse_attr, '\n')
-        print('T', final_title, '\n\n')
-
-    # print("unsorted")
-    # for x in html_array: print(x[0], x[1])
-    html_array = sorted(html_array, key = lambda x: (x[0], x[1]))
-    # print("\nsorted")
-    # for x in html_array: print(x[0], x[1], x[2], "\n")
+    with open(path, 'w') as f:
+        json.dump(content_array, f, indent=4)
